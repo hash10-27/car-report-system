d78e1bc414412e449934b8f41b9cc16cba0bdd3a
hash10-27 <sybrhashim@gmail.com>
2026-04-27 05:33:23 +0300

    remove python-bidi completely

--- arabic_fixer.py
+++ arabic_fixer.py
@@ -1,17 +1,7 @@
 import arabic_reshaper
 
-try:
-    from bidi.algorithm import get_display
-    BIDI_AVAILABLE = True
-except ImportError:
-    BIDI_AVAILABLE = False
-
-
 def normalize_arabic(text):
     try:
-        reshaped = arabic_reshaper.reshape(text)
-        if BIDI_AVAILABLE:
-            return get_display(reshaped)
-        return reshaped
+        return arabic_reshaper.reshape(text)
     except:
         return text
