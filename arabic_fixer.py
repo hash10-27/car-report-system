3c555146f1ad42278d90aaec7babccc03bcea148
hash10-27 <sybrhashim@gmail.com>
2026-04-27 04:59:00 +0300

    fix arabic_fixer for server

--- arabic_fixer.py
+++ arabic_fixer.py
@@ -1,12 +1,14 @@
 # arabic_fixer.py
 
-import arabic_reshaper
-from bidi.algorithm import get_display
+try:
+    import arabic_reshaper
+    from bidi.algorithm import get_display
 
-
-def normalize_arabic(text):
-    try:
+    def normalize_arabic(text):
         reshaped = arabic_reshaper.reshape(text)
         return get_display(reshaped)
-    except:
+
+except ImportError:
+    # 🔥 fallback إذا المكتبات غير موجودة (مثل Render)
+    def normalize_arabic(text):
         return text
